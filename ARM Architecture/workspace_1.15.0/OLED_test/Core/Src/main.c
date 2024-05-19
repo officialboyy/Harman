b0e48d77a0a4fb651dd85ef52a3cbe7fa81a0b21
officialboyy <pjh0063@gmail.com>
2024-05-19 14:17:17 +0900

    2024_05_19

--- ARM Architecture/workspace_1.15.0/OLED_test/Core/Src/main.c
+++ ARM Architecture/workspace_1.15.0/OLED_test/Core/Src/main.c
@@ -1,138 +1,257 @@
-#include "delay.h"
-#include "oled.h"
-#include "test.h"
-#include "bitmap.h"
-#include "horse_anim.h"
-#include "stdio.h"
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
-char zz[10];
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-	SSD1306_Init();
-	SSD1306_GotoXY (0,0);
 
-	SSD1306_Puts ("Embedded", &Font_7x10, 1);
+  /* USER CODE BEGIN 1 */
 
-	SSD1306_GotoXY (0, 10);
+  /* USER CODE END 1 */
 
-	SSD1306_Puts ("Expret.io", &Font_7x10, 1);
+  /* MCU Configuration--------------------------------------------------------*/
 
-	SSD1306_GotoXY (0, 30);
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	SSD1306_Puts ("OLED Display", &Font_7x10, 1);
+  /* USER CODE BEGIN Init */
 
-	SSD1306_GotoXY (0, 50);
+  /* USER CODE END Init */
 
-	SSD1306_Puts ("Bare Metal Code", &Font_7x10, 1);
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	SSD1306_UpdateScreen(); //display
+  /* USER CODE BEGIN SysInit */
 
-	delay (2000);
+  /* USER CODE END SysInit */
 
-	SSD1306_ScrollRight(0,7);  // scroll entire screen
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
 
-	delay(2000);  // 2 sec
+  /* USER CODE END 2 */
 
-	SSD1306_ScrollLeft(0,7);  // scroll entire screen
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
 
-	delay(2000);  // 2 sec
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
 
-	SSD1306_Stopscroll();
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	SSD1306_ScrollRight(0x00, 0x0f);    // scroll entire screen right
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	delay (2000);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	SSD1306_ScrollLeft(0x00, 0x0f);  // scroll entire screen left
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	delay (2000);
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
 
-	SSD1306_Scrolldiagright(0x00, 0x0f);  // scroll entire screen diagonal right
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
 
-	delay (2000);
+  /* USER CODE BEGIN USART2_Init 0 */
 
-	SSD1306_Scrolldiagleft(0x00, 0x0f);  // scroll entire screen diagonal left
+  /* USER CODE END USART2_Init 0 */
 
-	delay (2000);
+  /* USER CODE BEGIN USART2_Init 1 */
 
-	SSD1306_Stopscroll();   // stop scrolling. If not done, screen will keep on scrolling
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
 
-
-	SSD1306_InvertDisplay(1);   // invert the display
-
-	delay(2000);
-
-	SSD1306_InvertDisplay(0);  // normalize the display
-
-
-	delay(2000);
-	SSD1306_Clear();
-	SSD1306_GotoXY (0,0);
-	SSD1306_Puts ("variable", &Font_7x10, 1);
-	SSD1306_UpdateScreen();
-
-	for (int i=0; i<100;i++)
-	{
-	sprintf(zz,"%i",i);
-	SSD1306_GotoXY (10, 30);
-	SSD1306_Puts (zz, &Font_7x10, 1);
-	SSD1306_UpdateScreen();
-	delay(100);
-	}
-
-	while(1)
-	{
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse1,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse2,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse3,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse4,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse5,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse6,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse7,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse8,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse9,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-		SSD1306_Clear();
-		SSD1306_DrawBitmap(0,0,horse10,128,64,1);
-		SSD1306_UpdateScreen();
-		delay(1);
-
-	}
+  /* USER CODE END USART2_Init 2 */
 
 }
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
